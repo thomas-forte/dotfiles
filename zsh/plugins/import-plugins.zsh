d355b5ce3e96179ef8f77acf65606f400c1d06b8
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-05-30 13:34:52 -0400

    reordering plugins

--- zsh/plugins/import-plugins.zsh
+++ zsh/plugins/import-plugins.zsh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-antigen bundle zsh-users/zsh-autosuggestions
-
 antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply

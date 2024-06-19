10b7b43a9863cdc2d83237d82f6c2fd8e99a7629
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-06-18 22:45:01 -0400

    adding function from ohmyzsh

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -2,6 +2,21 @@
 
 # my favorite aliases that I got from ohmyzsh
 
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
+
 alias gf='git fetch'
 alias gfa='git fetch --all --prune'
 alias gcm='git checkout $(git_main_branch)'

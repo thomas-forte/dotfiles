744154b4df1c44231f2e4b88e69ae39c46df8563
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2026-07-27 12:31:47 -0400

    history update

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 HISTFILE="${XDG_DATA_HOME}/.zhistory"
-SAVEHIST=$(( 10 * 1000 ))
-HISTSIZE=$(( 1.2 * SAVEHIST ))
+SAVEHIST=1000000
+HISTSIZE=$(( SAVEHIST * 12 / 10 ))
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_expire_dups_first # delete duplicates first when SAVEHIST/HISTSIZE exceeds
+setopt hist_ignore_all_dups   # remove older duplicates when saving "move to newest position"
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data

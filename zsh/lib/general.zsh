81dbe803f1217eaece20eefe88e21682a4e17011
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2026-08-18 20:00:16 -0400

    remove bat overwrite

--- zsh/lib/general.zsh
+++ zsh/lib/general.zsh
@@ -5,11 +5,6 @@ alias config="cd $XDG_CONFIG_HOME"
 alias sshconfig="cat $HOME/.ssh/config"
 alias library="cd $HOME/Library"
 
-# alias over cat with bat
-if hash bat 2> /dev/null; then
-  alias cat='bat'
-fi
-
 # fun
 alias weather="curl 'wttr.in/Cleveland?0'"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
